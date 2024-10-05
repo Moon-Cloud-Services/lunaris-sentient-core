14878eb6b1d3bd6590546a93f74d2e75f0510e1a
Francisco Antonio <franciscoan@mooncloudpanel.xyz>
2024-10-05 19:04:40 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,15 +1,22 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from flask import Flask
 from app.controllers import traffic_controller, malware_controller
 from app.utils.encryption import Encryption
-from config.config import Config
 
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config['SECRET_KEY'] = 'your_secret_key_here'
 
 encryption = Encryption(app.config['SECRET_KEY'])
+
+@app.route("/")
+def home():
+    return {"message": "Lunaris Sentient Core is running"}
 
 app.register_blueprint(traffic_controller.bp)
 app.register_blueprint(malware_controller.bp)
 
 if __name__ == "__main__":
-    app.run(debug=app.config['DEBUG'], host=app.config['HOST'], port=app.config['PORT'])
+    app.run(debug=True)
