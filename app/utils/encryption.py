6071a1769912e80cfe9f73681da1ee0f84181c43
Francisco Antonio <franciscoan@mooncloudpanel.xyz>
2024-10-05 19:10:47 -0300

    Update encryption.py

--- app/utils/encryption.py
+++ app/utils/encryption.py
@@ -1,15 +1,14 @@
-from cryptography.fernet import Fernet
+# app/utils/encryption.py
 import base64
+from cryptography.fernet import Fernet
 
 class Encryption:
-    def __init__(self, key=None):
-        if key is None:
-            key = Fernet.generate_key()
+    def __init__(self, key):
         self.key = base64.urlsafe_b64encode(key.encode()[:32].ljust(32, b'\0'))
         self.cipher = Fernet(self.key)
 
     def encrypt_message(self, message):
-        return self.cipher.encrypt(message.encode()).decode()
+        return self.cipher.encrypt(message.encode())
 
     def decrypt_message(self, encrypted_message):
-        return self.cipher.decrypt(encrypted_message.encode()).decode()
+        return self.cipher.decrypt(encrypted_message).decode()
