b1a889eb6d45b35f583cc54438f6b4722b9df6e5
Francisco Antonio <franciscoan@mooncloudpanel.xyz>
2024-10-05 23:51:20 -0300

    Delete config directory

--- config/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-    HOST = os.environ.get('HOST', '0.0.0.0')
-    PORT = int(os.environ.get('PORT', 5000))
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key')
